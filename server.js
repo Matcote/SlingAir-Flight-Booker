c67e9355e50d659f0a368f1e527230e06b6eb200
Matcote <matthewcotesipod@gmail.com>
2020-07-15 14:24:05 -0400

    display which seats are taken properly

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { flights } = require('./test-data/flightSeating');
-
-const PORT = process.env.PORT || 8000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { flights } = require("./test-data/flightSeating");
 
 const handleFlight = (req, res) => {
-    const { flightNumber } = req.params;
-    // get all flight numbers
-    const allFlights = Object.keys(flights);
-    // is flightNumber in the array?
-    console.log('REAL FLIGHT: ', allFlights.includes(flightNumber))
-}
+  const { flightNumber } = req.params;
+  // get all flight numbers
+  const allFlights = Object.keys(flights);
+  // is flightNumber in the array?
+  console.log("REAL FLIGHT: ", allFlights.includes(flightNumber));
+  res.status(200).send(flights[flightNumber]);
+};
 
-{name: 'Fred'}
+//{name: 'Fred'}
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    
-    // endpoints
-    .get('/flights/:flightNumber', handleFlight)
-    .use((req, res) => res.send('Not Found'))
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("dev"))
+  .use(express.static("public"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+
+  // endpoints
+  .get("/flights/:flightNumber", handleFlight)
+  .use((req, res) => res.send("Not Found"))
+  .listen(8000, () => console.log(`Listening on port 8000`));
